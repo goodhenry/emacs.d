e9233e99ea68711299345ec07361af8789cb90e1
Yuanqi Cao <caoyuanqi@gmail.com>
2014-05-26 00:33:19 +1000

    Added auto complete clang support

--- lisp/init-auto-complete.el
+++ lisp/init-auto-complete.el
@@ -61,5 +61,41 @@
 
 (setq dabbrev-friend-buffer-function 'sanityinc/dabbrev-friend-buffer)
 
+;;; auto-complete-clang setting
+(require 'auto-complete-clang)
+(setq ac-clang-auto-save t)
+(setq ac-auto-start t)
+(setq ac-quick-help-delay 0.5)
+;; (ac-set-trigger-key "TAB")
+;; (define-key ac-mode-map  [(control tab)] 'auto-complete)
+(define-key ac-mode-map  [(control tab)] 'auto-complete)
+(defun my-ac-config ()
+  (setq ac-clang-flags
+        (mapcar(lambda (item)(concat "-I" item))
+               (split-string
+                "
+ /usr/local/Cellar/gcc49/4.9-20140302/lib/gcc/x86_64-apple-darwin13.1.0/4.9.0/include/c++
+ /usr/local/Cellar/gcc49/4.9-20140302/lib/gcc/x86_64-apple-darwin13.1.0/4.9.0/include/c++/x86_64-apple-darwin13.1.0
+ /usr/local/Cellar/gcc49/4.9-20140302/lib/gcc/x86_64-apple-darwin13.1.0/4.9.0/include/c++/backward
+ /usr/local/Cellar/gcc49/4.9-20140302/lib/gcc/x86_64-apple-darwin13.1.0/4.9.0/include
+ /usr/local/include
+ /usr/local/Cellar/gcc49/4.9-20140302/lib/gcc/x86_64-apple-darwin13.1.0/4.9.0/include-fixed
+ /usr/include
+ /System/Library/Frameworks
+ /Library/Frameworks
+")))
+  (setq-default ac-sources '(ac-source-abbrev ac-source-dictionary ac-source-words-in-same-mode-buffers))
+  (add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-mode-setup)
+  ;; (add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
+  (add-hook 'ruby-mode-hook 'ac-ruby-mode-setup)
+  (add-hook 'css-mode-hook 'ac-css-mode-setup)
+  (add-hook 'auto-complete-mode-hook 'ac-common-setup)
+  (global-auto-complete-mode t))
+(defun my-ac-cc-mode-setup ()
+  (setq ac-sources (append '(ac-source-clang ac-source-yasnippet) ac-sources)))
+(add-hook 'c-mode-common-hook 'my-ac-cc-mode-setup)
+;; ac-source-gtags
+(my-ac-config)
+
 
 (provide 'init-auto-complete)
