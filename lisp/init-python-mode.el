cf9d29206ef2082238d465202f7ec6788262f3fd
Yuanqi Cao <Cao@Caos-MacBook-Pro.local>
2014-11-21 22:35:52 +1100

    Require epc package

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -7,6 +7,7 @@
 		("SConscript\\'" . python-mode))
               auto-mode-alist))
 (require-package 'pip-requirements)
+(require-package 'epc)
 (require 'epcs)
 (require-package 'jedi)
 (add-hook 'python-mode-hook 'jedi:setup)
